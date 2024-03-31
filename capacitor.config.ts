78fa089afd2ff0a6cfa641ed563cf321eb80c295
Ingrid Alves <alves96.ingrid@gmail.com>
2024-03-31 12:21:00 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'av_formadora_comp01',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
